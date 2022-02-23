31d1f8338962183df4b2a84ae36bc0239661ba77
unknown <monoj93roy@gmailcom>
2022-02-23 11:49:02 +0600

    true us false

---
+++ trueFlase.js
@@ -0,0 +1,16 @@
+// falsely condition
+// 0;
+// "";
+// null;
+// Nah;
+// truety condition
+// "o", " ";
+// let names= null;
+// console.log(names);
+const names = 10;
+if(names || names==0){
+    console.log('condition is true')
+}
+else{
+    console.log('condition is false')
+}
