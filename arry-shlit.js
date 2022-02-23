8dccc9168ec7968620764e456e56866503cda298
unknown <monoj93roy@gmailcom>
2022-02-24 01:50:05 +0600

    all add

---
+++ arry-shlit.js
@@ -0,0 +1,8 @@
+const nums = [1,2,4,3,5,6,7,8,9]
+// const part = nums.slice(2,5);
+const removed= nums.splice(2,2, 77)
+// console.log(removed);
+// console.log(nums);
+
+const together = nums.join(' ')
+console.log(together);
---
+++ scope.js
@@ -0,0 +1,16 @@
+let bonus= 21;
+function sum(first, second){
+    let result = first + second + bonus;
+    console.log(result);
+    if(result>6){
+        var mood = 'happy';
+        mood ='fisehly'
+        // console.log(mood);
+    }
+    return result;
+    
+}
+
+
+const output = sum( 5, 6);
+// console.log(output);
