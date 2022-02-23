d5c25a670f56f7f80fb753457c5e27159ee2a557
unknown <monoj93roy@gmailcom>
2022-02-23 12:27:37 +0600

    nah-vs-undefined add

---
+++ nah-vs-undefined.js
@@ -0,0 +1,22 @@
+// undefined........
+// let pakhi;
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return;
+// }
+
+// function add ( num1, num2){
+//     console.log(num1,num2); 
+// }
+// const result = add (12)
+// console.log(result);
+
+// const names={name:'smart dude', phone:01745}
+// console.log(names.gf);
+
+// let fun = undefined;
+// console.log(fun);
+
+let ages=[12,13,45,25]
+console.log(ages[5]);
